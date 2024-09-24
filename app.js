02c5674d9f034e69d57a5bf2dc600ab92fd6958f
HARBAUGHDR <78834499+HARBAUGHDR@users.noreply.github.com>
2024-09-23 21:38:26 -0500

    106s1

---
+++ app.js
@@ -0,0 +1,25 @@
+
+function Hello() {
+console.log("Hello there!");
+}
+let golobal = 0; {
+    console.log()
+}
+function init() {
+    Hello();
+    let temp = 1;
+    let golbal = temp;
+console.log("hello we are talking about the DOM");
+}
+
+
+
+// variable scope 
+let i =10;
+for(let i = 0; i < 10, i++){
+    // empty variable
+}
+
+
+
+window.onload = init;
